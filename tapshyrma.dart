e9d17fa4e295324fd79dd66aa68aebab1a7fca67
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2022-12-28 23:56:34 +0600

    bmi_full

---
+++ tapshyrma.dart
@@ -0,0 +1,14 @@
+/*
+Game
+url: https://play.google.com/store/apps/details?id=com.asmolgam.capitals&hl=en_US&gl=US
+
+1) Айпери, Жылдыз Europa
+2) Айбике, Кубатбек Asia
+3) Айзада, Азима North America
+4) Арзымат, Нурали South America
+5) Бабаяр, Сыргы Africa
+6) Эржигит, Эржигит Australia and Oceania
+
+GridView
+url: https://api.flutter.dev/flutter/widgets/GridView-class.html
+ */
